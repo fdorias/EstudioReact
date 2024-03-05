448495fbb5c3322b22d9a996f0643a7ca9953a6d
Fernando <fernandoriosulloa@gmail.com>
2024-03-05 10:18:06 -0300

    Estudio React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+function holaMundo(nombre, edad) {
+  var presentacion = <div>
+  <h2>Hola soy {nombre}</h2>
+  <h3>Tengo {edad}</h3></div>
+  return presentacion;
+}
 
 function App() {
+  var nombre = 'Fernando Rios';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola  TODOS
         </p>
+        {holaMundo(nombre,12)}
+        {/* {alert('hOLA MUNDO CON REACT ')} */}
         <a
           className="App-link"
           href="https://reactjs.org"
